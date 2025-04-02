04cb54e2d0c2f9d8882643befc811ac78279d666
Sravani-kancham-123 <kanchamsravani3@gmail.com>
2025-04-02 21:25:02 +0530

    Update number-palindrome.js

--- number-palindrome.js
+++ number-palindrome.js
@@ -1,9 +1,14 @@
 let num = 123;
 let sum = 0;
 let rem;
+let temp = num;
 while(num!=0) {
     rem = num % 10;
     sum = sum*10+rem;
     num = Math.floor(num/10);
 }
-console.log(sum)
+if(temp == sum) {
+    console.log("the given number is palindrome")
+} else {
+    console.log("the given number is not a palindrome")
+}
